d0bfceb1bc482b6fe2b06bddb641ea2d74585b05
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-09-02 22:56:26 -0500

    changed file name.

---
+++ dev/codes/data_downloader_extractor.py
@@ -0,0 +1,25 @@
+# authors_name = 'Preetham Ganesh'
+# project_title = 'Comparison on approaches towards classification of CIFAR-100 dataset'
+# email = 'preetham.ganesh2015@gmail.com'
+
+
+import os
+import sys
+import logging
+
+import tensorflow_datasets as tfds
+import zipfile
+import tarfile
+import pandas as pd
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
