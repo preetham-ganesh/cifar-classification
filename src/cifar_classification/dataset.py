3d589d22fb7aafc0b4687e595a0eb834d659efdf
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2023-06-27 21:55:17 -0400

    added download_extract_data function.

--- src/cifar_classification/dataset.py
+++ src/cifar_classification/dataset.py
@@ -12,6 +12,11 @@ warnings.filterwarnings("ignore")
 
 
 from typing import Dict, Any
+import tensorflow_datasets as tfds
+import cv2
+
+from src.utils import check_directory_path_existence
+from src.utils import add_to_log
 
 
 class Dataset(object):
@@ -35,3 +40,44 @@ class Dataset(object):
 
         # Initalizes class variables.
         self.model_configuration = model_configuration
+
+    def download_extract_data(self, split: str) -> None:
+        """Downloads the CIFAR-100 dataset, and saves the image and label information.
+
+        Downloads the CIFAR-100 dataset, and saves the image and label information.
+
+        Args:
+            split: A string for the name of the current data split.
+
+        Returns:
+            None.
+
+        Exceptions:
+            OSError: When the directory requested to be deleted is not available.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(split, str), "Variable split should be of type 'str'."
+        assert split in [
+            "train",
+            "test",
+        ], "Variable split should have 'train' or 'test' as value."
+
+        self.home_directory_path = os.getcwd()
+
+        # Creates the following directory path if it does not exist.
+        raw_data_directory_path = check_directory_path_existence(
+            "data/raw_data/cifar_100/{}".format(split)
+        )
+
+        # Downloads cifar-100 dataset into the corresponding directory for the current data split.
+        dataset, info = tfds.load(
+            "cifar100",
+            split=split,
+            with_info=True,
+            shuffle_files=True,
+            data_dir=raw_data_directory_path,
+            as_supervised=True,
+        )
+        add_to_log("")
+        add_to_log("Downloaded the CIFAR-100 dataset for {} split.".format(split))
+        add_to_log("")
