e7e1ee2d11d497fa043110337a1dc995fdfc698d
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2023-07-23 20:38:11 -0400

    updated load_input_target_batches function.

--- src/cifar_classification/dataset.py
+++ src/cifar_classification/dataset.py
@@ -217,7 +217,6 @@ class Dataset(object):
         # Iterates across images & labels ids in current batch.
         input_batch, target_batch = list(), list()
         for index in range(self.model_configuration["batch_size"]):
-            print(type(image_ids[index]), type(label_ids[index]))
             print(str(image_ids[index]))
             # Loads the PNG image for the current image id as a NumPy array.
             image = self.load_image(str(image_ids[index], "UTF-8"))
